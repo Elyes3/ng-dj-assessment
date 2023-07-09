8e8f4670f7c35d3affa48e3990e43c364ac5849e
Elyes5 <elyes.saddem@etudiant-isi.utm.tn>
2023-07-09 12:57:53 +0200

    ALLOW_ALL

--- back-end/web_project/settings.py
+++ back-end/web_project/settings.py
@@ -51,8 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = ['*'
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'web_project.urls'
 
