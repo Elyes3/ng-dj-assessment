9482dd56fc1c60895b0225b83969a6f989ebd2d8
Elyes5 <elyes.saddem@etudiant-isi.utm.tn>
2023-07-09 12:41:41 +0200

    addedStatic

--- back-end/build.sh
+++ back-end/build.sh
@@ -2,4 +2,5 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 pip install gunicorn
+pip install psycopg2
 python manage.py migrate
--- back-end/labelers/urls.py
+++ back-end/labelers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import LabelListCreateAPIView,LabelListAPIView
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('labels/', LabelListCreateAPIView.as_view(), name='label-list-create'),
         path('labels/', LabelListAPIView.as_view(), name='label-list'),
 
 ]
+urlpatterns += staticfiles_urlpatterns();
